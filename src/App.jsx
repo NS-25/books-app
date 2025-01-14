668be8500c39a81b1fd61360173968c0e33c7bf7
George-25 <georgemallory26@gmail.com>
2025-01-13 23:48:11 -0500

    Book create file updated everything handlesubmit and state

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import BookCreate from "./components/BookCreate";
 function App() {
   let [books, setBooks] = useState([]);
 
-  const createBook = (title) => {};
+  const createBook = (title) => {
+    console.log("I am a :", title);
+  };
   return (
     <>
       <div>
