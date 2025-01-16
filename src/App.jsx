72f973f1c9d1afb13b599f3413aa698dbb87cce2
George-25 <georgemallory26@gmail.com>
2025-01-15 23:24:22 -0500

    Updated some codes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ function App() {
   let [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    const updatedBooks = [...books, { id: 123, title: title }];
+    const updatedBooks = [...books, { id: Math.round(Math.random() * 9999), title }];
     setBooks(updatedBooks);
   };
   return (
     <>
       <div>
+        {books.length}
         <BookCreate onCreate={createBook} />
       </div>
     </>
