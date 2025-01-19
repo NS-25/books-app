5cbb5774324783fbb7af89d14d0b7336ced28200
George-25 <georgemallory26@gmail.com>
2025-01-19 12:40:54 -0500

    added img tag for book lists

--- src/components/BookShow.jsx
+++ src/components/BookShow.jsx
@@ -28,6 +28,7 @@ const BookShow = ({ book, onDelete, onEdit }) => {
 
   return (
     <div className="book-show">
+      <img alt="boos" src="https://picsum.photos/300/200" />
       <div>{content}</div>
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
