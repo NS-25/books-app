5397c0328938df60c90f2128240fb7e3123b985e
George-25 <georgemallory26@gmail.com>
2025-01-18 21:13:56 -0500

    handlesubmit and update the form

--- src/components/BookEdit.jsx
+++ src/components/BookEdit.jsx
@@ -4,12 +4,19 @@ import React from "react";
 const BookEdit = () => {
   const [title, setTitle] = useState("");
 
+  // handle click for edit button...
   const handleChange = (event) => {
     setTitle(event.target.value);
   };
 
+  // handle submit for edit form..
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("New title", title);
+  };
+
   return (
-    <form className="book-edit">
+    <form onSubmit={handleSubmit} className="book-edit">
       <label>title</label>
       <input className="input" value={title} onChange={handleChange} />
       <button className="button is-primary">Save</button>
