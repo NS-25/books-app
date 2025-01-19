2fc8527ea2a0ef9a4e7993fbb1e7d6b01ac10573
George-25 <georgemallory26@gmail.com>
2025-01-19 12:49:31 -0500

    updated image with random pictures rendered

--- src/components/BookShow.jsx
+++ src/components/BookShow.jsx
@@ -28,7 +28,7 @@ const BookShow = ({ book, onDelete, onEdit }) => {
 
   return (
     <div className="book-show">
-      <img alt="boos" src="https://picsum.photos/300/200" />
+      <img alt="boos" src={`https://picsum.photos/seed/${book.id}/300/200`} />
       <div>{content}</div>
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
