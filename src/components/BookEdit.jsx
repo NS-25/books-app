ba20d21ac346494344cf0e263e1025f8a577f08c
George-25 <georgemallory26@gmail.com>
2025-01-18 21:07:06 -0500

    bookedit wrote some handle changes and input updates

--- src/components/BookEdit.jsx
+++ src/components/BookEdit.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import React from "react";
 
 const BookEdit = () => {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
   return (
     <form className="book-edit">
       <label>title</label>
-      <input className="input" />
+      <input className="input" value={title} onChange={handleChange} />
       <button className="button is-primary">Save</button>
     </form>
   );
