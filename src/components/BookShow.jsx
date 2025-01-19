5e588dbe324644d2748c0eebe922d1bdb6cd2106
George-25 <georgemallory26@gmail.com>
2025-01-18 20:21:24 -0500

    create edit button in bookShow jsx

--- src/components/BookShow.jsx
+++ src/components/BookShow.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
+
 import React from "react";
 
 const BookShow = ({ book, onDelete }) => {
+  const [showEdit, setShowEdit] = useState(false);
+  // handle delete function....
   const handleClick = () => {
     onDelete(book.id);
   };
+
   return (
     <div className="book-show">
       {book.title}
       <div className="actions">
+        <div className="action">
+          <button>Edit</button>
+        </div>
         <button className="delete" onClick={handleClick}>
           Delete
         </button>
