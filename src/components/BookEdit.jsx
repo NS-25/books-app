3fc53ac982c925a9d9b70274325574d4e4d3deb3
George-25 <georgemallory26@gmail.com>
2025-01-18 21:01:50 -0500

    book edit some updated stuffs

--- src/components/BookEdit.jsx
+++ src/components/BookEdit.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 const BookEdit = () => {
-  return <form className="book-edit">
-    <label>title</label>
-  </form>;
+  return (
+    <form className="book-edit">
+      <label>title</label>
+      <input className="input" />
+      <button className="button is-primary">Save</button>
+    </form>
+  );
 };
 
 export default BookEdit;
