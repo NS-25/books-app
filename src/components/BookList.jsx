80d67c9c8daa467b404a3c9c24521d481ee8a5d6
George-25 <georgemallory26@gmail.com>
2025-01-22 23:53:50 -0500

    some updated in booklist jsx

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,23 +3,15 @@ import { useContext } from "react";
 import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
-const BookList = ({ books, onDelete, onEdit }) => {
+const BookList = () => {
   // context value use here...
-  const { count, incrementCount } = useContext(BooksContext);
+  const { books } = useContext(BooksContext);
 
   const renderedBooks = books.map((book) => {
-    return (
-      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
-    );
+    return <BookShow key={book.id} book={book} />;
   });
 
-  return (
-    <div className="book-list">
-      {count}
-      <button onClick={incrementCount}>Click</button>
-      {renderedBooks}
-    </div>
-  );
+  return <div className="book-list">{renderedBooks}</div>;
 };
 
 export default BookList;
