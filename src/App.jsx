fc11e2e95f787d087a8979049df315618363161b
George-25 <georgemallory26@gmail.com>
2025-01-22 23:28:02 -0500

    some changes in book jsx of context

--- src/App.jsx
+++ src/App.jsx
@@ -14,39 +14,7 @@ function App() {
   // Do not call fetch request directly..
   // fetchBooks();
 
-  // Edit function for the book card
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
-
-    // console.log(response);
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...response.data };
-      }
-      return book;
-    });
-    setBooks(updatedBooks);
-  };
-
-  // delete function for the book card..
-  const deleteBookById = async (id) => {
-    await axios.delete(`http://localhost:3001/books/${id}`);
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-    setBooks(updatedBooks);
-  };
-
-  const createBook = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", {
-      title,
-    });
-
-    const updatedBooks = [...books, response.data];
-    setBooks(updatedBooks);
-  };
+ 
   return (
     <>
       <div className="app">
--- src/context/books.jsx
+++ src/context/books.jsx
@@ -10,6 +10,39 @@ function Provider({ children }) {
     const response = await axios.get("http://localhost:3001/books");
     setBooks(response.data);
   };
+  // Edit function for the book card
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    // console.log(response);
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, ...response.data };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
+
+  // delete function for the book card..
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
+    setBooks(updatedBooks);
+  };
 
   const [count, setCount] = useState(5);
 
